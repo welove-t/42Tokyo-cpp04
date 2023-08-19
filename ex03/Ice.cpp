1fdf2b65400cf752d913f11e08433ec93f9878f9
love <tlove.tky@gmail.com>
2023-08-20 08:53:32 +0900

    ex03: Iceクラスにuse追加

--- ex03/Ice.cpp
+++ ex03/Ice.cpp
@@ -30,3 +30,8 @@ AMateria*	Ice::clone() const
 {
 	return new Ice(*this);
 }
+
+void	Ice::use(ICharacter& target)
+{
+	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+}
